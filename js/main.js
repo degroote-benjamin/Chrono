68c367b7f42f3f6f0f09999226aa95371c42a402
benjamin <degroote.benjamin@gmail.com>
2017-07-20 16:18:17 +0200

    add bouton - + for each chrono

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-var timer = null,
-    interval = 1000,
-    value = 1000;
+var value = ($(".timerses").text())
 
-$(".chrono").click(function() {
-  if (timer !== null) return;
-  timer = setInterval(function () {
-      value = value-1;
-      $(".chrono").val(value);
-  }, interval);
+$(".chrono").text(($(".timerses").text())*60);
+
+$('.chrono').on('click', function() {
+
+  setInterval(function () {
+        value = value-1;
+    }, 1000);
+})
+
+$('.gaucheb').click(function(){
+    $(".timerbreak").text($(".timerbreak").text()-1)
 });
 
-$("#input").click(function() {
-  clearInterval(timer);
-  timer = null
+$('.droiteb').click(function(){
+    $(".timerbreak").text(parseInt($(".timerbreak").text())+1)
+});
+
+$('.gauches').click(function(){
+    $(".timerses").text($(".timerses").text()-1)
+});
+
+$('.droites').click(function(){
+    $(".timerses").text(parseInt($(".timerses").text())+1)
 });
