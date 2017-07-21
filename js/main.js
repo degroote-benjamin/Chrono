ef123b3d0c79072a7b51f51813216fb9b3e374e9
benjamin <degroote.benjamin@gmail.com>
2017-07-21 15:30:55 +0200

    clearinterval when you click on button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-var value = ($(".timerses").text()) * 60
+var value = ($(".timerses").text())
+var actuel = "default"
+var start = "stop"
+value = value * 60
+    var timer = setInterval(function() {
+        value = value - 1;
+        $(".chrono").text(sec(value));
+        swap()
+    }, 1000);
 
 $(".chrono").text(sec(value));
 
 $('.gaucheb').click(function() {
     $(".timerbreak").text($(".timerbreak").text() - 1)
-    if($(".timerbreak").text()<1){
-      $(".timerbreak").text(1)
+    if ($(".timerbreak").text() < 1) {
+        $(".timerbreak").text(1)
     }
 });
 
@@ -15,35 +23,40 @@ $('.droiteb').click(function() {
 
 $('.gauches').click(function() {
     $(".timerses").text($(".timerses").text() - 1)
-    if($(".timerses").text()<1){
-      $(".timerses").text(1)
+    if ($(".timerses").text() < 1) {
+        $(".timerses").text(1)
     }
     value = ($(".timerses").text()) * 60
     $(".chrono").text(sec(value));
+    clearInterval(timer)
 });
 
 $('.droites').click(function() {
     $(".timerses").text(parseInt($(".timerses").text()) + 1)
     value = ($(".timerses").text()) * 60
     $(".chrono").text(sec(value));
+    clearInterval(timer)
 });
-
 
 function sec(d) {
     d = Number(d);
     var m = Math.floor(d % 3600 / 60);
     var s = Math.floor(d % 3600 % 60);
-    if( m <10){
-      m = "0" + m
+    if (m < 10) {
+        m = "0" + m
     }
-    if(s<10){
-      s = "0" + s 
+    if (s < 10) {
+        s = "0" + s
     }
     return m + ":" + s
 }
 
-
-    setInterval(function() {
-        value = value - 1;
-        $(".chrono").text(sec(value));
-    }, 1000);
+function swap() {
+    if (actuel == "default" && value == 0) {
+        value = ($(".timerbreak").text()) * 60
+        actuel = "break"
+    } else if (actuel == "break" && value == 0) {
+        value = ($(".timerses").text()) * 60
+        actuel = "default"
+    }
+}
