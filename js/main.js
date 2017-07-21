bd36f5ab26885a4ff26405125698b3e143390273
benjamin <degroote.benjamin@gmail.com>
2017-07-21 10:26:00 +0200

    add interval -1 all 1sec

--- js/main.js
+++ js/main.js
@@ -1,26 +1,49 @@
-var value = ($(".timerses").text())
+var value = ($(".timerses").text()) * 60
 
-$(".chrono").text(($(".timerses").text())*60);
+$(".chrono").text(sec(value));
 
-$('.chrono').on('click', function() {
+$('.gaucheb').click(function() {
+    $(".timerbreak").text($(".timerbreak").text() - 1)
+    if($(".timerbreak").text()<1){
+      $(".timerbreak").text(1)
+    }
+});
 
-  setInterval(function () {
-        value = value-1;
+$('.droiteb').click(function() {
+    $(".timerbreak").text(parseInt($(".timerbreak").text()) + 1)
+});
+
+$('.gauches').click(function() {
+    $(".timerses").text($(".timerses").text() - 1)
+    if($(".timerses").text()<1){
+      $(".timerses").text(1)
+    }
+    value = ($(".timerses").text()) * 60
+    $(".chrono").text(sec(value));
+});
+
+$('.droites').click(function() {
+    $(".timerses").text(parseInt($(".timerses").text()) + 1)
+    value = ($(".timerses").text()) * 60
+    $(".chrono").text(sec(value));
+});
+
+
+function sec(d) {
+    d = Number(d);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    if( m <10){
+      m = "0" + m
+    }
+    if(s<10){
+      s = "0" + s 
+    }
+    return m + ":" + s
+}
+
+
+    setInterval(function() {
+        value = value - 1;
+        $(".chrono").text(sec(value));
     }, 1000);
-})
-
-$('.gaucheb').click(function(){
-    $(".timerbreak").text($(".timerbreak").text()-1)
-});
-
-$('.droiteb').click(function(){
-    $(".timerbreak").text(parseInt($(".timerbreak").text())+1)
-});
-
-$('.gauches').click(function(){
-    $(".timerses").text($(".timerses").text()-1)
-});
-
-$('.droites').click(function(){
-    $(".timerses").text(parseInt($(".timerses").text())+1)
-});
