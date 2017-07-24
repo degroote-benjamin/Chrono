7a80ebe33729e8831f58bb3d8164939bd9c043e5
benjamin <degroote.benjamin@gmail.com>
2017-07-24 15:38:47 +0200

    add animation , new class and css

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ var value = ($(".timerses").text())
 var actuel = "default"
 var timer = null
 value = value * 60
+$('.session').css('background-color', 'blue');
 
 $("#bonjour").click(function() {
     if ($("#bonjour").hasClass("chrono")) {
+          animationcolor()
         if (timer !== null) {
             return;
         }
         timer = setInterval(function() {
             value = value - 1;
-            $("#bonjour").text(sec(value));
+            $(".timer").text(minute(value));
             swap()
         }, 1000);
         $("#bonjour").toggleClass('chrono chronos');
@@ -18,13 +20,13 @@ $("#bonjour").click(function() {
         clearInterval(timer)
         timer = null
         $("#bonjour").toggleClass('chronos chrono');
+        $('.session').stop()
     }
 })
 
 
-
 // init val in chrono
-$("#bonjour").text(sec(value));
+$(".timer").text(minute(value));
 
 
 // break button
@@ -35,21 +37,20 @@ $('.gaucheb').click(function() {
     }
     if (actuel === "break") {
         value = ($(".timerbreak").text()) * 60
-        $("#bonjour").text(sec(value));
+        $(".timer").text(minute(value));
         clearInterval(timer)
         timer = null
-        $("#bonjour").toggleClass('chronos chrono');
-    }
+$("#bonjour").removeClass("chronos").addClass("chrono")    }
 });
 
 $('.droiteb').click(function() {
     $(".timerbreak").text(parseInt($(".timerbreak").text()) + 1)
     if (actuel === "break") {
         value = ($(".timerbreak").text()) * 60
-        $("#bonjour").text(sec(value));
+        $(".timer").text(minute(value));
         clearInterval(timer)
         timer = null
-        $("#bonjour").toggleClass('chronos chrono');
+        $("#bonjour").removeClass("chronos").addClass("chrono")
     }
 });
 
@@ -62,27 +63,25 @@ $('.gauches').click(function() {
     }
     if (actuel === "default") {
         value = ($(".timerses").text()) * 60
-        $("#bonjour").text(sec(value));
+        $(".timer").text(minute(value));
         clearInterval(timer)
         timer = null
-        $("#bonjour").toggleClass('chronos chrono');
-    }
+$("#bonjour").removeClass("chronos").addClass("chrono")    }
 });
 
 $('.droites').click(function() {
     $(".timerses").text(parseInt($(".timerses").text()) + 1)
     if (actuel === "default") {
         value = ($(".timerses").text()) * 60
-        $("#bonjour").text(sec(value));
+        $(".timer").text(minute(value));
         clearInterval(timer)
         timer = null
-        $("#bonjour").toggleClass('chronos chrono');
-    }
+$("#bonjour").removeClass("chronos").addClass("chrono")    }
 });
 
 
 // function for minute
-function sec(d) {
+function minute(d) {
     d = Number(d);
     var m = Math.floor(d % 3600 / 60);
     var s = Math.floor(d % 3600 % 60);
@@ -101,8 +100,23 @@ function swap() {
     if (actuel == "default" && value == 0) {
         value = ($(".timerbreak").text()) * 60
         actuel = "break"
+        $('.session').css('background-color', 'red');
+        $('.session').width('0px')
+        $('.session').height('0px')
+        animationcolor()
     } else if (actuel == "break" && value == 0) {
         value = ($(".timerses").text()) * 60
         actuel = "default"
+        $('.session').css('background-color', 'blue');
+        $('.session').width('0px')
+        $('.session').height('0px')
+        animationcolor()
     }
+}
+
+function animationcolor() {
+        $('.session').animate({
+            height: '100%',
+            width: '100%',
+        }, (value * 1000));
 }
